581d3dece9f572e462fc2ae0caa3fab0e365c491
Daewoong(Andy) Jun <70481532+dwjun0905@users.noreply.github.com>
2022-09-13 21:45:35 -0700

    Add files via upload

---
+++ amazon-review-sentiment-analysis/app/utils.py
@@ -0,0 +1,4 @@
+
+DATA_DIR = "/Users/daewoongjun/Desktop/Programming/Python/DaewoongJun_Portfolio/amazon-review-sentimental-analysis/data"
+MODEL_DIR = "/Users/daewoongjun/Desktop/Programming/Python/DaewoongJun_Portfolio/amazon-review-sentimental-analysis/model"
+IMG_DIR = "/Users/daewoongjun/Desktop/Programming/Python/DaewoongJun_Portfolio/amazon-review-sentimental-analysis/app/static/images"
